4df92dc90501eb89052f1e5eb8fe197b8bf0cfbe
jk <1004561881@qq.com>
2017-07-20 19:17:08 +0800

    Initial commit

---
+++ src/models/products.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jk on 17-7-20.
+ */
+import dva from 'dva';
+
+export default {
+  namespace: 'products',
+  state: [],
+  reducers: {
+    'delete'(state, { payload: id }) {
+      return state.filter(item => item.id !== id);
+    },
+  },
+};
